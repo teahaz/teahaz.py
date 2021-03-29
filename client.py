a5f0c6b6cd1fae55b645f008e45526c4b385084b
thomasthethermonuclearbomb <morrissbestyen@gmail.com>
2021-03-30 00:52:04 +0200

    added symetric encryption and keygen for encrypting strings
    also added __example.py as a demonstration of how to use it

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import threading
 import requests
+import string
 import base64
 import pickle
 import time
@@ -19,6 +20,22 @@ def decrypt_message(a):
 
 def decrypt_binary(a):
     return base64.b64decode(str(a).encode('utf-8'))
+
+def _gen_token(a):
+    "Generating token out of password, so It cannot be reversed"
+    return base64.urlsafe_b64encode(str(hashlib.sha256(a.encode('utf-8')).digest())[:32].encode('utf-8'))
+
+def _encrpt_string(string, token):
+    "Encrypts a string given to it"
+    f = Fernet(token)
+    enc = f.encrypt(string.encode('utf-8'))
+    return enc.decode('utf-8')
+
+def _decrypt_string(encrypted_string, token):
+    "Decrypts a string given to it"
+    f = Fernet(token)
+    dec = f.decrypt(encrypted_string.encode('utf-8'))
+    return dec.decode('utf-8')
 
 def sanitize_filename(a):
     allowed = string.ascii_letters + string.digits + '_-.'
